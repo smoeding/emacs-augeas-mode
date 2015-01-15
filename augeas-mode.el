3476ad5a3f717f6dfacee6fbfe672f255725d828
Stefan Möding <stm@kill-9.net>
2015-01-15 17:00:59 +0100

    Improve font-lock

--- augeas-mode.el
+++ augeas-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Stefan Moeding <stm@kill-9.net>
 ;; URL: https://github.com/smoeding/emacs-augeas-mode
-;; Time-stamp: <2015-01-14 20:30:40 stm>
+;; Time-stamp: <2015-01-15 17:00:19 stm>
 ;; Keywords: languages
 ;; Version: 0.0.1
 
@@ -85,13 +85,15 @@
 (defvar augeas-font-lock-keywords
   `((,augeas-keywords-regexp . font-lock-keyword-face)
     (,augeas-builtin-regexp . font-lock-builtin-face)
-    ("\\<Util\\.[A-Za-z_]+\\>" . font-lock-constant-face)
+    ("\\<\\(module\\|test\\) +\\([A-Z][A-Za-z0-9_]*\\)\\>"
+     2 font-lock-function-name-face)
+    ("\\<[A-Z][A-Za-z0-9_]*\\.[a-z][A-Za-z0-9_]+\\>" . font-lock-constant-face)
     ("\\<[a-z][A-Za-z0-9_]*\\>" . font-lock-variable-name-face))
   "Keywords used to highlight in Augeas mode.")
 
 (defalias 'augeas-parent-mode
   (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode)
-  "The mode used as parent mode for `augeas-mode'.
+  "The major mode used as parent mode for `augeas-mode'.
 
 Either `prog-mode' if available or `fundamental-mode' otherwise.")
 
